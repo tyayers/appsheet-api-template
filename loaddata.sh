9ef5c418140884280082eebcb062c3951c468035
tyayers <tyler.ayers@gmail.com>
2022-03-08 10:46:46 +0100

    updated data import script

--- loaddata.sh
+++ loaddata.sh
@@ -1,24 +1,40 @@
 export TOKEN=$(gcloud auth print-access-token)
 
-curl --location --request POST "https://firestore.googleapis.com/v1/projects/$GOOGLE_CLOUD_PROJECT/databases/(default)/documents/notes?documentId=b78dfdb" \
---header "Authorization: Bearer $TOKEN" \
---header "Content-Type: application/json" \
---data-raw "{
-    'fields': {
-        'id': {
-            'stringValue': 'b78dfdb'
-        },
-        'name': {
-            'stringValue': 'Test note'
-        },
-        'text': {
-            'stringValue': 'This is a test note'
-        },
-        'location': {
-            'stringValue': 'Berlin, Germany'
-        },
-        'likes': {
-            'integerValue': 7
+export COUNTER=0
+
+while [ $COUNTER -le 5 ]
+do
+    RESULT=$(curl --location --request POST "https://firestore.googleapis.com/v1/projects/$GOOGLE_CLOUD_PROJECT/databases/(default)/documents/notes?documentId=b78dfdb" \
+    --header "Authorization: Bearer $TOKEN" \
+    --header "Content-Type: application/json" \
+    --data-raw "{
+        'fields': {
+            'id': {
+                'stringValue': 'b78dfdb'
+            },
+            'name': {
+                'stringValue': 'Test note'
+            },
+            'text': {
+                'stringValue': 'This is a test note'
+            },
+            'location': {
+                'stringValue': 'Berlin, Germany'
+            },
+            'likes': {
+                'integerValue': 7
+            }
         }
-    }
-}"
+    }")
+
+    echo $RESULT
+
+    if [[ "$RESULT" == *error* ]]
+    then
+        COUNTER=$(( $COUNTER + 1 ))
+    else
+        COUNTER=6
+    fi
+
+    sleep 2s
+done
